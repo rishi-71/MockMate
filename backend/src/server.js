5b5657acb13141e78a9feeda007f3ae1e086fa32
rishi <rishikeshpatole234@gmail.com>
2026-01-28 00:17:16 +0530

    setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,28 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
+
 const app = express();
 
-app.get("/",(req,res)=>{
+const __dirname = path.resolve();
+
+app.get("/health",(req,res)=>{
     res.status(200).json({
-        msg: "Success from backend"
+        msg:"api is up and running"
+    });
+});
+app.get("/books",(req,res)=>{
+    res.status(200).json({
+        msg: "Success from books"
     })
 });
+
+//make our app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 app.listen(ENV.PORT, ()=> console.log("Server is running on port 3000"));
