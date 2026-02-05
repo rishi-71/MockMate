41a70d59c321dfd92ce3a6be1b17f86ad7437f91
rishi <rishikeshpatole234@gmail.com>
2026-02-06 00:08:50 +0530

    Login working + token received

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 export const registerUser = async (req,res) =>{
@@ -28,6 +29,45 @@ export const registerUser = async (req,res) =>{
     }catch(err){
         res.status(500).json({
             message: err.message
+        });
+    }
+};
+
+export const loginUser = async (req,res)=>{
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({
+                message:"Invalid credentials"
+            });
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"Invalid credentials"
+            });
+
+        }
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        );
+        res.json({
+            token,
+            user:{
+                id: user._id,
+                name:user.name,
+                email:user.email
+            }
+        });
+
+    }catch(err){
+        res.status(500).json({
+            message:err.message
         });
     }
 };
---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req,res,next)=>{
+    let token = req.headers.authorization;
+
+    if(token && token.startswith("Bearer")){
+        token = token.split(" ")[1];
+
+        try{
+            const decoded = jwt.verify(token,process.env.JWT_SECRET);
+            req.user = decoded.id;
+            next();
+        }catch{
+            return res.status(401).json({
+                message:"Not authorized"
+            });
+        }
+        
+    }else{
+        return res.status(401).json({
+            message:"No token"
+        });
+    }
+};
